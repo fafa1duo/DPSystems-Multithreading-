fa99aada4e2a90822f460a48d0a907468f853de3
fafa1duo <2454390419@qq.com>
2024-02-19 20:09:01 +0300

    first commit.Lesson01. Task 01. How to create and run a thread

---
+++ lessono1-task02-run-thread/src/by/bsu/lesson01tsak2/HelloRunnable.java
@@ -0,0 +1,13 @@
+package by.bsu.lesson01tsak2;
+
+public class HelloRunnable implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String[] args) {
+        (new Thread(new HelloRunnable())).start();
+    }
+
+}
